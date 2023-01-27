1b11c4b1d70a780ac05b180603fc8817f8d90beb
fghovde <fghovde@gmail.com>
2023-01-27 09:21:14 +0100

    Typo

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from '../components/layout/Layout'
+import Layout from '../components/Layout/Layout'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
