88ef0dd2eacd9062420c5e475ccff244572b768f
fghovde <fghovde@gmail.com>
2023-01-27 09:16:06 +0100

    new config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
 }
 
 module.exports = nextConfig
