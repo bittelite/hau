bb0a7067fccbfbe502e5a46ef12da7fa71c1cc40
fghovde <fghovde@gmail.com>
2023-03-03 08:28:03 +0100

    Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/uwx7pki.css" />
+        <script 
+          async 
+          src={"https://www.googletagmanager.com/gtag/js?id=G-HCLEWMMHXD"}
+        /> 
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date()); 
+          gtag('config', 'G-HCLEWMMHXD', {
+            page_path: window.location.pathname,
+          });
+          `,
+         }}
+        />
       </Head>
       <body>
         <Main />
